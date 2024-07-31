f35a74ec449bb5a1fd39bec886bb450791bd3554
Idder Ait El Mouden <139082259+IDDER29@users.noreply.github.com>
2024-08-01 00:28:25 +0100

    extracte sum data from the amazon product website

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
+import { extractCurrency, extractPrice } from "../utils";
 export async function scrapeAmazonProduct(url: string) {
   // Scrape the product page
   if (!url) return;
@@ -21,7 +22,61 @@ export async function scrapeAmazonProduct(url: string) {
   try {
     // Fetch the product page
     const response = await axios.get(url, options);
-    console.log(response.data);
+    const $ = cheerio.load(response.data);
+
+    // Extract the product title
+    const title = $("#productTitle").text().trim();
+    const currentPrice = extractPrice(
+      $(".priceToPay span.a-price-whole"),
+      $("a.size.base.a-color-price"),
+      $(".a-button-selected .a-color-base")
+    );
+
+    const originalPrice = extractPrice(
+      $("#priceblock_ourPrice"),
+      $(".a-price.a-text-price span.a-offscreen"),
+      $("#listPrice"),
+      $("#priceblock_dealprice"),
+      $(".a-size-base.a-color-price")
+    );
+
+    const outOfStock =
+      $("#availability span").text().trim().toLocaleLowerCase() ===
+      "curently unavailable";
+
+    const images =
+      $("#imgBlkFront").attr("data-a-dynamic-image") ||
+      $("#landingImage").attr("data-a-dynamic-image") ||
+      "{}";
+    const imageUrls = Object.keys(JSON.parse(images));
+    const currency = extractCurrency($(".a-price-symbol"));
+    const discountRate = $(".savingsPercentage").text().replace(/ [-%]/g, "");
+    console.log({
+      title,
+      currentPrice,
+      originalPrice,
+      outOfStock,
+      imageUrls,
+      currency,
+      discountRate,
+    });
+
+    // Construct the product object with scraped information
+
+    const data = {
+      url,
+      currency: currency || "$",
+      image: imageUrls[0],
+      title,
+      currentPrice: Number(currentPrice) || "N/A",
+      originalPrice: Number(originalPrice) || "N/A",
+      priceHistory: [],
+      discountRate: Number(discountRate),
+      category: "category",
+      isOutOfStock: outOfStock,
+      reviewsCount: 100,
+      stars: 4.5,
+    };
   } catch (error) {
     throw new Error("Failed to scrape Amazon product");
   }
